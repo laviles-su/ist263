8b21de85c61aa2932751eddf3f8f772ab62bb1f6
laviles-SU <laviles@syr.edu>
2025-11-12 16:15:23 -0500

    done

---
+++ participation/participation12.js
@@ -0,0 +1,6 @@
+var orangeDiv = document.getElementById("orangeDiv");
+
+orangeDiv.onclick = function() {
+    orangeDiv.innerHTML = "Orange Nation";   // change text
+    orangeDiv.style.color = "orange";       // change text color
+};
